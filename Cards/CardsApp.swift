0a59a42a0484b388c5b11da96ca4b5d3948cfd7c
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2025-02-05 09:13:56 +0545

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Roshan Bade on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
