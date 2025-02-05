3099a71249e5c747114fa0bde2a9d7223ce85048
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2025-02-05 12:43:23 +0545

    Skills: drag gesture; operator overloading

--- Cards/Views/ResizableView.swift
+++ Cards/Views/ResizableView.swift
@@ -10,14 +10,30 @@ import SwiftUI
 struct ResizableView: View {
     
     @State private var transform = Transform()
+    @State private var previousOffset: CGSize = .zero
     
     private let content = RoundedRectangle(cornerRadius: 20)
     private let color = Color.red
+    
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                transform.offset = CGSize(
+                    width: value.translation.width + previousOffset.width,
+                    height: value.translation.height + previousOffset.height
+                )
+            }
+            .onEnded { _ in
+                previousOffset = transform.offset
+            }
+    }
     
     var body: some View {
         content
             .frame(width: transform.size.width, height: transform.size.height)
             .foregroundColor(color)
+            .offset(transform.offset)
+            .gesture(dragGesture)
     }
 }
 
