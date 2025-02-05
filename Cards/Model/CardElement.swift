ef35a375fd35cb4b79414cdd3ea5c72098b9b422
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2025-02-05 17:24:54 +0545

    Protocols: create protocol; conform structures to protocol; protocol method

--- Cards/Model/CardElement.swift
+++ Cards/Model/CardElement.swift
@@ -8,6 +8,28 @@
 import Foundation
 import SwiftUI
 
-struct CardElement {
-    
+protocol CardElement {
+    var id : UUID { get }
+    var transform: Transform { get set }
 }
+
+extension CardElement {
+    func index(in array: [CardElement]) -> Int? {
+        array.firstIndex(where: { $0.id == id })
+    }
+}
+
+struct ImageElement: CardElement {
+    var id = UUID()
+    var transform = Transform()
+    var image: Image
+}
+
+struct TextElement: CardElement {
+    var id = UUID()
+    var transform = Transform()
+    var text: String = ""
+    var textColor = Color.black
+    var textFont = "Gill Sans"
+}
+
