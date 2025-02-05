ea62fa420bbf3d6fb2b1ca5664e70210eeffac2d
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2025-02-05 13:09:33 +0545

    rotation gesture

--- Cards/Views/ResizableView.swift
+++ Cards/Views/ResizableView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ResizableView: View {
     
     @State private var transform = Transform()
+    @State private var previousRotation: Angle = .zero
     @State private var previousOffset: CGSize = .zero
     
     private let content = RoundedRectangle(cornerRadius: 20)
@@ -25,12 +26,25 @@ struct ResizableView: View {
             }
     }
     
+    var rotationGesture: some Gesture {
+        RotationGesture()
+            .onChanged { rotation in
+                transform.rotation += rotation - previousRotation
+                previousRotation = rotation
+            }
+            .onEnded { _ in
+                previousRotation = .zero
+            }
+    }
+    
     var body: some View {
         content
             .frame(width: transform.size.width, height: transform.size.height)
             .foregroundColor(color)
+            .rotationEffect(transform.rotation)
             .offset(transform.offset)
             .gesture(dragGesture)
+            .gesture(rotationGesture)
     }
 }
 
